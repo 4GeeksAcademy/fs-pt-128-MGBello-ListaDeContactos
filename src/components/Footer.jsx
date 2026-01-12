2c093c8ba418e926d48d2a48ec96ec5d739b8be4
MBello21 <magarbello@gmail.com>
2026-01-12 03:56:18 +0000

    correccion class > className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="footer text-light mt-auto py-3 text-center position-absolute bottom-0 start-0 end-0 bg-dark">
       <p className="m-0">
         <a href="https://4geeksacademy.slack.com/team/U0985A80P24">
-          <i class="fa-solid fa-copyright mx-1"></i>2026 Migue Garcia Bello</a>
+          <i className="fa-solid fa-copyright mx-1"></i>2026 Migue Garcia Bello</a>
       </p>
     </footer>
   );
